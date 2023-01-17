e9eb5144ea4ece5402b7a81443d03ad1fbba763e
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-01-17 12:56:58 +0330

    feat: config prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/strongly-recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  plugins: ['prettier'],
 }
