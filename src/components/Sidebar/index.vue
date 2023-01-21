1d65cb270935f1663fc44de5edf67aec5e559b2b
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-01-21 11:47:46 +0330

    feat: sidebar ui

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,12 +1,18 @@
 <template>
   <NLayoutSider content-style="padding: 8px">
-    <NMenu :options="menuOptions" :default-expanded-keys="['apis']" :indent="0" inverted />
+    <NMenu
+      inverted
+      :indent="0"
+      class="sidebar__menu"
+      :options="menuOptions"
+      :default-expanded-keys="['apis']"
+    />
   </NLayoutSider>
 </template>
 
 <script lang="ts">
-import { defineComponent, h as createVNode } from 'vue'
 import type { MenuOption } from 'naive-ui'
+import { defineComponent, h as createVNode } from 'vue'
 import { NButton, NLayoutSider, NMenu, NSpace, NTag } from 'naive-ui'
 import { AddOutline as IconAdd, LayersOutline as IconLayers } from '@vicons/ionicons5'
 
@@ -28,7 +34,11 @@ const menuOptions: MenuOption[] = [
         key: 'api-01',
         label: () =>
           createVNode(NSpace, {}, [
-            createVNode(NTag, { bordered: false, type: 'success' }, 'POST'),
+            createVNode(
+              NTag,
+              { bordered: false, type: 'success', class: 'sidebar__menu-item-tag' },
+              'POST'
+            ),
             '/user',
           ]),
       },
@@ -36,7 +46,11 @@ const menuOptions: MenuOption[] = [
         key: 'api-02',
         label: () =>
           createVNode(NSpace, {}, [
-            createVNode(NTag, { bordered: false, type: 'info' }, 'GET'),
+            createVNode(
+              NTag,
+              { bordered: false, type: 'info', class: 'sidebar__menu-item-tag' },
+              'GET'
+            ),
             '/user/256',
           ]),
       },
@@ -52,8 +66,13 @@ const menuOptions: MenuOption[] = [
           createVNode(
             NButton,
             { block: true, size: 'large' },
-            { default: 'Add New Schema', icon: () => createVNode(IconLayers) }
+            { default: 'Add New Schema', icon: () => createVNode(IconAdd) }
           ),
+      },
+      {
+        key: 'schema-user',
+        label: 'User',
+        icon: () => createVNode(IconLayers, null, null),
       },
     ],
   },
@@ -64,8 +83,6 @@ export default defineComponent({
 
   components: {
     NMenu,
-    NButton,
-    IconAdd,
     NLayoutSider,
   },
 
